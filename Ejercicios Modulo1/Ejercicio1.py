d3881d1e63a8da768eaeeb11a068556beea13ab3
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-06-01 10:12:50 +0200

    ejercicio1

---
+++ Ejercicios Modulo1/Ejercicio1.py
@@ -0,0 +1,13 @@
+# 1- Declaración de las variables
+gravedad = 9.81           # float
+material = "Acero"        # str
+muestras = 15             # int
+paso_prueba = True        # bool
+
+# 2- Imprimir en pantalla los valores y el tipo de dato
+
+print(f'Aceleracion de la gravedad: {gravedad}', type(gravedad))
+print(f'MAterial analizado: {material}', type(material))
+print(f'Número de muestras: {muestras}', type(muestras))
+print(f'Pasó la Prueba el material?: {paso_prueba}', type(paso_prueba))
+
